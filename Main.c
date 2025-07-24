cdaed4d4affeb24bc8baeef87ddef04f07a87a0c
Daniel Guerra-Rojas <danielguerra135@gmail.com>
2025-07-23 23:58:06 -0700

    Changed title of file containing Main function

---
+++ Main.c
@@ -0,0 +1,21 @@
+/**********************************************************/
+/* Main file to run program							      */
+/*														  */
+/* Author: Daniel Guerra-Rojas							  */
+/*														  */
+/**********************************************************/
+
+// Started 7/23/25
+
+#include "GameSet.h"
+#include "BoardEdit.h"
+#include <stdio.h>
+
+int main()
+{
+	printmenu();
+
+	return 0;
+}
+
+/* EOF */
